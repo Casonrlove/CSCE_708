8fa30da67f045b63b627fc709c337e3702db3e32
casonrlove <casonrlove@tamu.edu>
2024-02-01 15:58:08 -0600

    update

--- assignments/NameGameEx2.java
+++ assignments/NameGameEx2.java
@@ -7,31 +7,35 @@ public class NameGameEx2
     public static void main(String[] args) 
     {
         
-        //-----------------------Header-----------------------//
+        //-----------------------HEADER-----------------------//
         System.out.println("The name game!\n\n\n");
         System.out.println("Come on now everybody, I said lets play a game.");
         System.out.println("I bet'cha I could make a rhyme");
         System.out.println("Out of anybody's name...\n\n\n");
 
-        //-----------------------Scanner Name In-----------------------//
+
+        //-----------------------TAKE NAME AS INPUT-----------------------//
         System.out.print("What is your name? ");
         Scanner scnr = new Scanner(System.in);
         String user_name = scnr.nextLine();
+        
 
-        //-----------------------Check if my name-----------------------//
+        //-----------------------CHECK IF MY NAME-----------------------//
         if((user_name == "Cason") || (user_name == "cason"))
         {
-            System.out.println("Howdy " + user_name + " thats my name too!");
+            System.out.println("\nHowdy " + user_name + " thats my name too!");
         }
         else
         {
-            System.out.println("Howdy " + user_name); 
+            System.out.println("\nHowdy " + user_name); 
         }
         
-        //---------------------------Print----------------------------//
-        System.out.println("Okay " + user_name + ", here is your rhyme:");
 
-        //-----------------------Set Conditions-----------------------//
+        //---------------------------PRINT----------------------------//
+        System.out.println("\n\n\nOkay " + user_name + ", here is your rhyme:\n");
+
+
+        //-----------------------SET CONDITIONS-----------------------//
         boolean first_letter_vowel      = false;
         boolean first_letter_consanant  = false;
         boolean last_letter_vowel       = false;
@@ -39,10 +43,14 @@ public class NameGameEx2
         boolean is_double_consanant     = false;
         boolean is_double_vowel         = false;
 
+
         //----------FIRST LETTER----------//
         if( (user_name.charAt(0) == 'a')    ||      (user_name.charAt(0) == 'e')    ||
             (user_name.charAt(0) == 'i')    ||      (user_name.charAt(0) == 'o')    ||
-            (user_name.charAt(0) == 'u')    ||      (user_name.charAt(0) == 'a'))
+            (user_name.charAt(0) == 'u')    ||      (user_name.charAt(0) == 'a')    ||
+            (user_name.charAt(0) == 'A')    ||      (user_name.charAt(0) == 'E')    ||
+            (user_name.charAt(0) == 'I')    ||      (user_name.charAt(0) == 'O')    ||
+            (user_name.charAt(0) == 'U'))
         {
             first_letter_vowel = true;
         }
@@ -50,10 +58,14 @@ public class NameGameEx2
         {
             first_letter_consanant = true;
         }
+
         //----------SECOND LETTER----------//
         if( (user_name.charAt(1) == 'a')    ||      (user_name.charAt(1) == 'e')    ||
             (user_name.charAt(1) == 'i')    ||      (user_name.charAt(1) == 'o')    ||
-            (user_name.charAt(1) == 'u')    ||      (user_name.charAt(1) == 'a'))
+            (user_name.charAt(1) == 'u')    ||      (user_name.charAt(1) == 'a')    ||
+            (user_name.charAt(1) == 'A')    ||      (user_name.charAt(1) == 'E')    ||
+            (user_name.charAt(1) == 'I')    ||      (user_name.charAt(1) == 'O')    ||
+            (user_name.charAt(1) == 'U'))
         {
             last_letter_vowel = true;
         }
@@ -61,69 +73,94 @@ public class NameGameEx2
         {
             last_letter_consanant = true;
         }
-        //--------------------Check for Doubles--------------------//
-        if()
 
+        //--------------------CHECK FOR DOUBLES--------------------//
+        if(first_letter_vowel && last_letter_vowel)
+        {
+            is_double_vowel = true;
+        }
+        else if (first_letter_consanant && last_letter_consanant) 
+        {
+            is_double_consanant = true;
+        }
 
-        //--------------------Condition Statements--------------------//
-        //----------Single Consanant----------//
-        if(1)
+        //--------------------DEBUG--------------------//
+        // System.out.println("Double Vowel = " + is_double_vowel);
+        // System.out.println("Double Cons = " + is_double_consanant);
+        
+
+        //--------------------CONDITION STATEMENTS--------------------//
+        //----------DOUBLE CONSANTS----------//
+        if(is_double_consanant)
         {
-            System.out.println(
-                    "Sarah, Sarah, Bo-barah\n" + //
-                    "Bo-na-na fana Fo-farah Fee\n" + //
-                    "fi Mo-marah, Sarah!");
+            String second_string = user_name.substring(2);
+            System.out.printf( 
+                    "%s, %s, Bo-b%s\nBo-na-na fana Fo-f%s\nFee fi Mo-m%s, %s!\n", 
+                    user_name, user_name, second_string, second_string, second_string, user_name);
         }
-        //----------Single Vowel----------//
-        else if(1)
+        //----------DOUBLE VOWELS----------//
+        else if(is_double_vowel)
         {
-            System.out.println( 
-                    "Anna, Anna, Bo-banna\n" + //
-                    "Bo-na-na fana Fo-fana Fee\n" + //
-                    "fi Mo-manna, Anna!");
+            String second_string = user_name.substring(1);
+            System.out.printf(
+                    "%s, %s, Bo-b%s\n" + //
+                    "Bo-na-na fana Fo-f%s\n" + //
+                    "Fee fi Mo-m%s, %s!",
+                    user_name, user_name, second_string, second_string, second_string, user_name);
         }
-        //----------Double Consanant----------//
-        else if(1)
+        //----------B AS FIRST LETTER----------//
+        else if((user_name.charAt(0) == 'B') ||(user_name.charAt(0) == 'b'))
         {
-            System.out.println( 
-                    "Thomas, Thomas, Bo-bomas\n" + //
-                    "Bo-na-na fana Fo-fomas\n" + //
-                    "Fee fi Mo-momas, Thomas!");
-        }
-        //----------Double Vowel----------//
-        else if(1)
-        {
-            System.out.println(
-                    "Aaron, Aaron, Bo-baron\n" + //
-                    "Bo-na-na fana Fo-faron\n" + //
-                    "Fee fi Mo-maron, Aaron!");
-        }
-        //----------B as first letter----------//
-        else if(1)
-        {
-            System.out.println(
+            String second_string = user_name.substring(2);
+            System.out.printf(
                     "Bob, Bob, Bo-ob Bo-na-\n" + //
                     "na fana Fo-fob Fee fi\n" + //
                     "Mo-mob, Bob!");
         }
-        //----------F as first letter----------//
-        else if(1)
+        //----------F AS FIRST LETTER----------//
+        else if((user_name.charAt(0) == 'F') ||(user_name.charAt(0) == 'f'))
         {
-            System.out.println(
-                    "Felix, Felix, Bo-belix\n" + //
-                    "Bo-na-na fana Fo-elix Fee\n" + //
-                    "fi Mo-melix, Felix!");
+            String second_string = user_name.substring(1);
+            System.out.printf(
+                    "%s, %s, Bo-b%s\n" + //
+                    "Bo-na-na fana Fo-%s Fee\n" + //
+                    "fi Mo-m%s, %s!", 
+                    user_name, user_name, second_string, second_string, second_string, user_name);
         }
-        //----------M as first letter----------//
+        //----------M AS FIRST LETTER----------//
+        else if((user_name.charAt(0) == 'M') ||(user_name.charAt(0) == 'm'))
+        {
+            String second_string = user_name.substring(1);
+            System.out.printf(
+                    "%s, %s, Bo-b%s\n" + //
+                    "Bo-na-na fana Fo-f%s Fee\n" + //
+                    "fi Mo-%s, %s!",
+                    user_name, user_name, second_string, second_string, second_string, user_name);
+        }
+        //----------SINGLE CONSANT----------//
+        else if(first_letter_consanant)
+        {
+            String second_string = user_name.substring(1);
+            System.out.printf(
+                    "%s, %s, Bo-b%s\n" + //
+                    "Bo-na-na fana Fo-f%s Fee\n" + //
+                    "fi Mo-m%s, %s!",
+                    user_name, user_name, second_string, second_string, second_string, user_name);
+        }
+        //----------SINGLE VOWEL----------//
+        else if(first_letter_vowel)
+        {
+            String second_string = user_name.toLowerCase();
+            System.out.printf( 
+                    "%s, %s, Bo-b%s\n" + //
+                    "Bo-na-na fana Fo-fana Fee\n" + //
+                    "fi Mo-m%s, %s!",
+                    user_name, user_name, second_string, second_string, user_name);
+        }
         else
         {
-            System.out.println(
-                    "Mary, Mary, Bo-bary\n" + //
-                    "Bo-na-na fana Fo-fary Fee\n" + //
-                    "fi Mo-ary, Mary!");
+            System.out.println("Something went wrong");
         }
-
-
 
 
     }
