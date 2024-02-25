d366c7f19948848e505b028653e2cb360bc669a5
casonrlove <casonrlove@tamu.edu>
2024-02-25 13:26:35 -0600

    a

--- practice/output.java
+++ practice/output.java
@@ -1,10 +1,16 @@
 public class output {
-    public static void main(String[] args) {
-        System.out.print(addMethod(7, 2));
+    public static void main (String[] args) {
+        int x;
+        int y;
+        x = 10;
+        y = 20;
+        swap(x,y);
+        System.out.println(x + ", " + y);
     }
-    public static int addMethod(int m, int n) {
-        int sum;
-        sum = m + n;
-        return sum;
+    public static void swap(int val1, int val2) {
+        int temp;
+        temp = val1;
+        val1 = val2;
+        val2 = temp;
     }
 }
