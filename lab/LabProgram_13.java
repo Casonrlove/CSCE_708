407aeaab11b11f35b016c3bfb9037b55091883a2
casonrlove <casonrlove@tamu.edu>
2024-03-04 14:26:50 -0600

    yep

---
+++ lab/LabProgram_13.java
@@ -0,0 +1,44 @@
+/*
+ * Lab 8.1 (Arrays/Loops/If)
+ * Directions:
+ *    Given a sorted list of integers, output the middle integer. A negative number indicates the end of the input 
+ *    (the negative number is not a part of the sorted list). Assume the number of integers is always odd.
+ *    The maximum number of list values for any test case should not exceed 9. If exceeded, output “Too many numbers.”
+ * 
+ * Ex: If the Input is:
+ *    2 3 4 8 11 -1
+ * The output is:
+ *    Middle Item: 4
+ * 
+ * Hint: First read data into an array. Then, based on the array’s size, find the middle item.
+ */
+
+public class LabProgram_13 {
+   public static void main(String[] args) {
+
+
+
+
+      
+   //    Scanner scnr = new Scanner(System.in);
+   //    int[] userValues = new int[9];  // Set of data specified by the user      
+   //    int numValues = 0; //number of values inputted by user
+   //    int inputValue = scnr.nextInt(); //taking the input
+
+   //    while (inputValue >= 0) {
+   //       if (numValues >= 9) { //checking to see if the amount of inputted numbers is too much
+   //           System.out.println("Too many numbers\n");
+   //           return;
+   //       }
+
+   //       userValues[numValues] = inputValue; //setting the array at the proper location to the input
+   //       numValues++; //adding that there was an input
+   //       inputValue = scnr.nextInt(); //setting input value for next run in the loop
+   //   }
+
+   //   int middleIndex = numValues / 2; //can divide by two ince its -1 to get to the proper indexing
+   //   int middleValue = userValues[middleIndex]; //set value equal to the middle
+
+   //   System.out.println("Middle item: " + middleValue + "\n"); //output
+   }
+}
